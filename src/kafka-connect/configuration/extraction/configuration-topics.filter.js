229ca0de44682e1a207053093bf09bb1e24ed5f6
Nick Amoscato <nick.amoscato@jazz.co>
2018-02-04 21:47:08 -0500

    Fix filter filenames

--- src/kafka-connect/configuration/extraction/configuration-topics.filter.js
+++ src/kafka-connect/configuration/extraction/configuration-topics.filter.js
@@ -1,29 +1,61 @@
 (function() {
   'use strict';
 
+  var TOPIC_OPTION_REGEX = /(?:kafka[._])?topics?(?:[._]prefix)?$/;
+
   /**
-   * Extracts the type from the connector configuration
-   * @requires supportedConnectorsFactory
+   * Extracts the topics from the connector configuration
+   * @requires configurationTypeFilter
    */
-  angularAPP.filter('configurationType', function (supportedConnectorsFactory) {
+  angularAPP.filter('configurationTopics', function (configurationTypeFilter) {
 
     /**
      * @param {Object} config Connector configuration
-     * @returns {String} Connector type (`Source` or `Sink`)
+     * @returns {Array} Set of topic strings
      */
     return function (config) {
-      var configClass = config['connector.class'];
-      var configClassParts;
-      var connector = supportedConnectorsFactory.getSupportedConnectorObj(configClass);
+      var option;
+      var topics;
 
-      if (connector.type) {
-        return connector.type;
+      if (angular.isString(config.topics) && 'Sink' === configurationTypeFilter(config)) {
+        return trimWhiteSpace(config.topics.split(','));
       }
 
-      configClassParts = configClass.split('.');
+      for (option in config) {
+        if (angular.isString(config[option]) && TOPIC_OPTION_REGEX.test(option)) {
+          return trimWhiteSpace(config[option].split(','));
+        }
+      }
 
-      return -1 !== configClassParts[configClassParts.length - 1].toLowerCase().indexOf('source') ? 'Source' : 'Sink';
+      topics = [];
+
+      for (option in config) {
+        if (!angular.isString(config[option])) {
+          continue;
+        }
+
+        if (-1 !== option.indexOf('ftp.monitor')) { // @see {@link https://github.com/Eneco/kafka-connect-ftp}
+          Array.prototype.push.apply(topics, config[option].split(',').map(function (value) {
+            return value.split(':')[1].trim();
+          }));
+        } else if (-1 !== option.indexOf('topic')) {
+          Array.prototype.push.apply(topics, trimWhiteSpace(config[option].split(',')));
+        }
+      }
+
+      return topics;
     };
   });
+
+  /**
+   * Removes whitespace from the set of strings
+   * @param {Array} topics
+   * @returns {Array}
+   */
+  function trimWhiteSpace(topics) {
+    return topics.map(function (topic) {
+      return topic.trim();
+    });
+  }
 
 })();
--- src/kafka-connect/configuration/extraction/configuration-type.filter.js
+++ src/kafka-connect/configuration/extraction/configuration-type.filter.js
@@ -1,61 +1,29 @@
 (function() {
   'use strict';
 
-  var TOPIC_OPTION_REGEX = /(?:kafka[._])?topics?(?:[._]prefix)?$/;
-
   /**
-   * Extracts the topics from the connector configuration
-   * @requires configurationTypeFilter
+   * Extracts the type from the connector configuration
+   * @requires supportedConnectorsFactory
    */
-  angularAPP.filter('configurationTopics', function (configurationTypeFilter) {
+  angularAPP.filter('configurationType', function (supportedConnectorsFactory) {
 
     /**
      * @param {Object} config Connector configuration
-     * @returns {Array} Set of topic strings
+     * @returns {String} Connector type (`Source` or `Sink`)
      */
     return function (config) {
-      var option;
-      var topics;
+      var configClass = config['connector.class'];
+      var configClassParts;
+      var connector = supportedConnectorsFactory.getSupportedConnectorObj(configClass);
 
-      if (angular.isString(config.topics) && 'Sink' === configurationTypeFilter(config)) {
-        return trimWhiteSpace(config.topics.split(','));
+      if (connector.type) {
+        return connector.type;
       }
 
-      for (option in config) {
-        if (angular.isString(config[option]) && TOPIC_OPTION_REGEX.test(option)) {
-          return trimWhiteSpace(config[option].split(','));
-        }
-      }
+      configClassParts = configClass.split('.');
 
-      topics = [];
-
-      for (option in config) {
-        if (!angular.isString(config[option])) {
-          continue;
-        }
-
-        if (-1 !== option.indexOf('ftp.monitor')) { // @see {@link https://github.com/Eneco/kafka-connect-ftp}
-          Array.prototype.push.apply(topics, config[option].split(',').map(function (value) {
-            return value.split(':')[1].trim();
-          }));
-        } else if (-1 !== option.indexOf('topic')) {
-          Array.prototype.push.apply(topics, trimWhiteSpace(config[option].split(',')));
-        }
-      }
-
-      return topics;
+      return -1 !== configClassParts[configClassParts.length - 1].toLowerCase().indexOf('source') ? 'Source' : 'Sink';
     };
   });
-
-  /**
-   * Removes whitespace from the set of strings
-   * @param {Array} topics
-   * @returns {Array}
-   */
-  function trimWhiteSpace(topics) {
-    return topics.map(function (topic) {
-      return topic.trim();
-    });
-  }
 
 })();
